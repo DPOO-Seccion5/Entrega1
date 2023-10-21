bb1264420f43591d9f5241b68d19d2c4edd2a76d
Juaneslot01 <78174625+Juaneslot01@users.noreply.github.com>
2023-10-21 14:11:02 -0500

    Todos los Atributos

--- Entrega3/src/modelo/AdministradorGeneral.java
+++ Entrega3/src/modelo/AdministradorGeneral.java
@@ -1,5 +1,16 @@
 package modelo;
 
+import procesamiento.Compañia;
+
 public class AdministradorGeneral {
+	
+	private Compañia compañia;
+	
+	
+	public AdministradorGeneral(Compañia laCompañia)
+	{
+		this.compañia = laCompañia;
+	}
+	
 
 }
--- Entrega3/src/procesamiento/Compañia.java
+++ Entrega3/src/procesamiento/Compañia.java
@@ -1,5 +1,26 @@
 package procesamiento;
 
+import java.util.List;
+
+import modelo.*;
+
 public class Compañia {
+	
+	
+	private Inventario inventario;
+	
+	private List<Sede> sedes;
+	
+	private List<Cliente> clientes;
+	
+	
+	public Compañia(Inventario elInventario, List<Sede> lasSedes, List<Cliente> losClientes)
+	{
+		this.inventario = elInventario;
+		this.sedes = lasSedes;
+		this.clientes = losClientes;
+	}
+	
+	
 
 }
