f4cd3f69483eddea9514e2fecc1999f5b483efbf
Juaneslot01 <78174625+Juaneslot01@users.noreply.github.com>
2023-10-21 15:39:12 -0500

    Clase Administrador local

---
+++ Entrega3/src/modelo/AdministradorLocal.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import java.util.List;
+
+public class AdministradorLocal {
+
+	
+	private List<Empleado> empleados;
+	
+	private List<Sede> sedes;
+	
+	private List<Cliente> clientes;
+
+}
